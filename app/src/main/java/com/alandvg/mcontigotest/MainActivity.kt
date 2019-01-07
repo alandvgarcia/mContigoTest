de5893eeeb797046cdf46d4306dcb15220180dec
Alan Garcia <alan.garcia@solinftec.com.br>
2019-01-06 23:43:55 -0200

    iniciando testes com api

--- app/src/main/java/com/alandvg/mcontigotest/MainActivity.kt
+++ app/src/main/java/com/alandvg/mcontigotest/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.alandvg.mcontigotest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.alandvg.mcontigotest.api.ItunesApi
+import com.alandvg.mcontigotest.entity.SearchResult
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +15,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        ItunesApi.itunesService().searchMusicVideo("beyonce").enqueue(object : Callback<SearchResult> {
+            override fun onFailure(call: Call<SearchResult>, t: Throwable) {
+                Log.d("Teste", "erro")
+            }
+
+            override fun onResponse(call: Call<SearchResult>, response: Response<SearchResult>) {
+                Log.d("Teste", response.body().toString())
+
+
+                response.body()?.results?.forEach { jsonElement ->
+
+                }
+
+
+            }
+
+
+
+        })
+
 
 
     }
